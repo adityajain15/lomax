1b08008eb6f60bdc224f6dfdaeb1f44100d3c53c
Aditya J <ajain15@uic.edu>
2018-03-14 16:38:34 -0400

    public assets path fixed

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/lomax',
+    assetsPublicPath: 'lomax',
     proxyTable: {},
 
     // Various Dev Server settings
