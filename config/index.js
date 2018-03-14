07e95ea4aaec78a6ae1ed327420ea3c8ab7e4b56
Aditya J <ajain15@uic.edu>
2018-03-14 17:47:09 -0400

    meh

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'lomax/static',
-    assetsPublicPath: '',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '..lomax/',
 
     /**
      * Source Maps
